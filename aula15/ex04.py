14b16a6e59484516686c0848b3c17918c1d07b7c
FelipeMachad0 <94871489+FelipeMachad0@users.noreply.github.com>
2022-01-18 16:51:24 -0300

    bug aula 15 ex 04 corrigido

--- aula15/ex04.py
+++ aula15/ex04.py
@@ -2,19 +2,23 @@ maior_idade = m_sexo = f_idade20 = 0
 while True:
     print('-'*20)
     idade = int(input('IDADE: '))
-    sexo = str(input('SEXO: [M/F] ')).upper()[0]
+    while True:
+        sexo = str(input('SEXO: [M/F] ')).upper()[0]
+        if sexo == 'M' or sexo == 'F':
+            break
+        print('Gênero inválido')
     if idade >= 18:
         maior_idade += 1
     if sexo == 'M':
         m_sexo += 1
-    if sexo == 'F' and idade > 20:
+    if sexo == 'F' and idade < 20:
         f_idade20 += 1
     print('-'*20)
     while True:
         continuar = str(input('QUER CONTINUAR? [S/N] ')).upper()[0]
         if continuar == 'S' or continuar == 'N':
             break
-        print('Gênero inválido')
+        print('Opção inválida')
     if continuar == 'N':
         break
 print(f'Tem {maior_idade} maiores de idade')
