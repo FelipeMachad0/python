bb21d8b65049aa6efe5724d2a8aa6cfe5ad0112a
FelipeMachad0 <94871489+FelipeMachad0@users.noreply.github.com>
2022-01-25 14:49:31 -0300

    aula 16 ex 02 feito

--- aula16/ex02.py
+++ aula16/ex02.py
@@ -1,4 +1,5 @@
-cont = cont2 = cont3 = 0
+cont = cont2 = cont4 = cont5 = 0
+cont3 = 16
 times = ['Atlético-MG', 'Flamengo', 'Palmeiras', 'Fortaleza', 'Corinthians', 'Bragrantino', 'Fluminense', 'América-MG', 'Atlético-GO', 'Santos', 'Ceará', 'Internacional', 'São Paulo', 'Athletico-PR', 'Cuiabá', 'Juventude', 'Grêmio', 'Bahia', 'Sport', 'Chapecoense']
 print('Tabela Brasileirão 2021:', end = ' ')
 while True:
@@ -18,7 +19,24 @@ while True:
 print('=-' * 30)
 print('Z4:', end = ' ')
 while True:
-    print(times[-cont3], end=', ')
+    print(times[cont3], end=', ')
     cont3 += 1
-    if cont3 > 3:
+    if cont3 > 19:
+        print('')
         break
+print('=-' * 30)
+print('Ordem álfabetica:', end = ' ')
+while True:
+    if times[cont4] == 'Chapecoense':
+        chape_posicao = cont4 + 1
+        break
+    cont4 += 1
+times.sort()
+while True:
+    print(times[cont5], end=', ')
+    cont5 += 1
+    if cont5 == 19:
+        print('')
+        break
+print('=-' * 30)
+print(f'A Chapecoense ficou na posição {chape_posicao}')
