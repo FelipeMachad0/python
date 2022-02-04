20b4e4264beca6914815c5c4c11ec7805d99e8d2
FelipeMachad0 <94871489+FelipeMachad0@users.noreply.github.com>
2022-02-04 12:21:40 -0300

    adicionado features

--- aula19/ex02.py
+++ aula19/ex02.py
@@ -1,9 +1,12 @@
 from random import randint
+from time import sleep
 cont = 1
+print('Dados jogados!!!')
 dados = {'Pessoa 1': randint(1,6), 
 'Pessoa 2': randint(1,6), 
 'Pessoa 3': randint(1,6),
 'Pessoa 4': randint(1,6)}
 for item in sorted(dados, key = dados.get, reverse = True):
+    sleep(1)
     print (f'{cont}º lugar: {item} com {dados[item]}.')
     cont += 1
